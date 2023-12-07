3a9476d73f2751501355d731951528e6f34fa3c9
Griffin Clark <56231413+griffinclark10@users.noreply.github.com>
2023-12-07 15:51:59 -0800

    added correct start and end time formatting

---
+++ astro/common/fakedata.ts
@@ -0,0 +1,27 @@
+
+
+export const headers = ["Key", "Title", "Status", "Voting Start", "Voting End", "Results"];
+
+
+export const rows = [
+  {
+    "key": "MIP4",
+    "category": "Best Picture",
+    "title": "Nomadland",
+    "status": "Completed",
+    "votingStart": 1678881600000,
+    "votingEnd": 1704067200000,
+    "resultsLink": "https://www.oscars.org/oscars/ceremonies/2021"
+
+  },
+  {
+    "key": "MIP1",
+    "category": "Best Actor",
+    "title": "Anthony Hopkins",
+    "status": "In Progress",
+    "votingStart": 1656927000000,
+    "votingEnd": 1703529900000,
+    "resultsLink": "https://www.oscars.org/oscars/ceremonies/2021"
+  }
+]
+
