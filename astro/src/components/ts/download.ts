c3f86944ef35ef9415d5109a15e8812a8a527aee
Griffin Clark <56231413+griffinclark10@users.noreply.github.com>
2024-01-18 18:15:22 -0800

    download to csv working, excel not working

---
+++ astro/src/components/ts/download.ts
@@ -0,0 +1,64 @@
+import * as xslx from 'xslx';
+
+export default function downloadFile(data: string, format: string) {
+    let fileContent;
+    let mimeType;
+    let fileExtension;
+
+    const dataArray = JSON.parse(data) as Array<{ [key: string]: any }>;
+
+    if (format === 'csv') {
+        fileContent = convertToCSV(dataArray);
+        mimeType = 'text/csv';
+        fileExtension = 'csv';
+    } else if (format === 'xslx') {
+        // Conversion for xslx (requires a library or a custom function)
+        fileContent = convertToxslx(dataArray);
+        mimeType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+        fileExtension = 'xslx';
+    } else {
+        console.error('Unsupported format');
+        return;
+    }
+
+    // Create a Blob from the data
+    const blob = new Blob([fileContent], { type: mimeType });
+
+    // Create a link and trigger the download
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = `data.${fileExtension}`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+function convertToCSV(data: any[]) {
+    try {
+        
+        if (!Array.isArray(data)) {
+            throw new Error("Parsed JSON is not an array.");
+        }
+        const rows = data.map(item => Object.values(item).join(','));
+        return rows.join('\n');
+    } catch (error) {
+        console.error('Error parsing JSON string:', error);
+        return '';
+    }
+}
+
+function convertToxslx(data: any[]) {
+    try {
+        if (!Array.isArray(data)) {
+            throw new Error("Parsed JSON is not an array.");
+        }
+        const workbook = xslx.utils.book_new();
+        const worksheet = xslx.utils.json_to_sheet(data);
+
+        xslx.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+        return xslx.write(workbook, { bookType: 'xslx', type: 'array' });
+    } catch (error) {
+        console.error('Error processing data:', error);
+    }
+}
+
