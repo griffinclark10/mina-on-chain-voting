a92dfbb95b539c5bce88cb8d927ea869a74c7dee
Griffin Clark <56231413+griffinclark10@users.noreply.github.com>
2024-01-20 16:57:40 -0800

    search filtration complete

--- astro/src/components/ts/filtration.ts
+++ astro/src/components/ts/filtration.ts
@@ -11,11 +11,12 @@ export function filtration(url: URL, rows: ProposalType | VoteType) {
     const status = url.searchParams.getAll('Status');
     const category = url.searchParams.getAll('Category');
     const direction = url.searchParams.getAll('Vote');
+    const search = url.searchParams.getAll('search'); 
 
     if (isProposalType(rows)) {
-        return filterRowType(rows, status, category);
+        return filterRowType(rows, status, category, search);
     } else if (isVoteType(rows)) {
-        return filterVoteType(rows, status, direction);
+        return filterVoteType(rows, status, direction, search);
     }
 
     return [];
@@ -37,11 +38,19 @@ function isVoteType(rows: any[]): rows is VoteType {
     return rows.every(row => 'direction' in row && 'status' in row);
 }
 
-function filterRowType(rows: ProposalType, status: string[], category: string[]): ProposalType {
-    return filterItems(rows, { status, category });
+function filterRowType(rows: ProposalType, status: string[], category: string[], search: string[]): ProposalType {
+    let filteredRows = filterItems(rows, { status, category });
+    if (search[0] && search[0] !== '') {
+        filteredRows = filteredRows.filter(row => row.description.includes(search[0]));
+    }
+    return filteredRows;
 }
 
-function filterVoteType(votes: VoteType, status: string[], direction: string[]): VoteType {
-    return filterItems(votes, { status, direction });
+function filterVoteType(votes: VoteType, status: string[], direction: string[], search: string[]): VoteType {
+    let filteredRows = filterItems(votes, { status, direction });
+    if (search[0] && search[0] !== '') {
+        filteredRows = filteredRows.filter(row => row.account.includes(search[0]));
+    }
+    return filteredRows;
 }
 
